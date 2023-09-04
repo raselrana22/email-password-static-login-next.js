eb18a6903faab5f2bd9b861e50ed1db959484d8a
Rasel Rana <rasel.learn22@gmail.com>
2023-09-04 23:02:20 +0600

    The middleware function working

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { verifyTokenCookie } from "./app/lib/verify-token-cookie";
 
 export async function middleware(req) {
-    if (req.nextUrl.pathname.startsWith('/dashboard')) {
-        return await verifyTokenCookie(req);
+    try {
+        if (req.nextUrl.pathname.startsWith('/dashboard')) {
+            // Verify the token cookie
+            const verified = await verifyTokenCookie(req);
+
+            if (verified) {
+                // Token is valid, allow access to the protected route
+                return NextResponse.next();
+            } else {
+                // Token is invalid, deny access
+                return NextResponse.redirect('/login'); // Redirect to a login page or handle unauthorized access appropriately
+            }
+        }
+
+        // Allow access to other routes
+        return NextResponse.next();
+    } catch (error) {
+        // Handle any errors that occur during token verification
+        console.error("Middleware error:", error);
+        return NextResponse.error(new Error("An error occurred during middleware execution"));
     }
 }
